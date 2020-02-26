8bdd4b08fe5dd58f55f2870e573e2cafa67e1c29
tearill <1105317666@qq.com>
2020-02-26 22:23:21 +0800

    生命周期函数学习

---
+++ Vue/vue-cli-learn/src/views/LifeCycle.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h3 id="h3">{{ msg }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "ok"
+    };
+  },
+  methods: {
+    show() {
+      console.log("执行了show方法");
+    }
+  },
+  beforeCreate() {
+    console.log(this.msg); // undefined
+  },
+  created() {
+    // 遇到的第二生命周期函数 --- data 和 methods 都已经被初始化好了
+    console.log(this.msg);
+    this.show();
+  },
+  beforeMount() {
+    // 模板已经在内存中编译完成
+    // console.log(document.getElementById('h3').innerText);
+  },
+  
+};
+</script>
+
+<style>
+</style>
