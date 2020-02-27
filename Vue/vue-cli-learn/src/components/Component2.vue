6a5740e0fd5070ae7d928c5020e6a64001788586
tearill <1105317666@qq.com>
2020-02-27 23:15:39 +0800

    自定义组件2

---
+++ Vue/vue-cli-learn/src/components/Component2.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input type="text" v-model="val">
+    <button @click="submit">提交给父组件</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Component2",
+  props: ['text', 'msg'],
+  model: {
+    prop: 'msg',
+    event: 'on-callback'
+  },
+  data() {
+    return {
+      val: this.msg
+    }
+  },
+  methods: {
+    submit() {
+      // val
+      this.$emit('on-callback', this.val)
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
