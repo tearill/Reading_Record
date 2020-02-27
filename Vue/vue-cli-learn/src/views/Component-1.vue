4f6af96a89d6a4717d35f662c5c23eebad53c2a2
tearill <1105317666@qq.com>
2020-02-27 23:23:00 +0800

    定义组件和组件属性传参

---
+++ Vue/vue-cli-learn/src/views/Component-1.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>组件学习</h1>
+
+    <!-- <my-component></my-component> -->
+    <!-- <my-component text="组件传参"></my-component> -->
+    <hr />
+    <my-component :text="msg" :num="20" :obj="obj" :list="list" :bool="bool"></my-component>
+  </div>
+</template>
+
+<script>
+import MyComponent from "../components/Component1.vue";
+export default {
+  components: {
+    MyComponent
+  },
+  data() {
+    return {
+      msg: "动态参数",
+      obj: {
+        a: 1,
+        b: 2
+      },
+      list: [1, 2, 3, 4, 5],
+      bool: false
+    };
+  }
+};
+</script>
+
+<style>
+</style>
