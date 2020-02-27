f76e37cd1ab6289f8ecc862b76b3f64be038c63e
tearill <1105317666@qq.com>
2020-02-27 23:21:27 +0800

    slot示例组件

---
+++ Vue/vue-cli-learn/src/components/SlotDemo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2 class="title">{{title}}</h2>
+    <slot>默认</slot>
+    <slot name="btns"></slot>
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SlotDemo',
+  props: ['title']
+}
+</script>
+
+<style>
+
+</style>
