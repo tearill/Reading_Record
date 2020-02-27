4fa9c0e02472243b72d0ef582dd15f147808dcca
tearill <1105317666@qq.com>
2020-02-27 23:25:33 +0800

    子组件和父组件通信--修改父组件的状态

---
+++ Vue/vue-cli-learn/src/views/Component-2.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>子组件与父组件通信</h1>
+
+    <my-component :text="content" @on-callback="getData"></my-component>
+    <div>{{content}}</div>
+
+    <hr />
+
+    <my-component v-model="content"></my-component>
+    <div>{{content}}</div>
+  </div>
+</template>
+
+<script>
+import MyComponent from "../components/Component2.vue";
+export default {
+  data() {
+    return {
+      content: "hello"
+    };
+  },
+  components: {
+    MyComponent
+  },
+  methods: {
+    getData(val) {
+      // console.log(val);
+      this.content = val;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
