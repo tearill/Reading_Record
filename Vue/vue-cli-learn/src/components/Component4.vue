850721f2870bb2ee5303285cc52374a745696a1e
tearill <1105317666@qq.com>
2020-03-01 23:13:05 +0800

    组件4

---
+++ Vue/vue-cli-learn/src/components/Component4.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>我是子组件</h2>
+    <button @click="btnClick">子组件的按钮</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Component4",
+  data() {
+    return {
+      name: '我是子组件的data'
+    }
+  },
+  methods: {
+    btnClick() {
+      // $parent 访问父组件  
+      console.log(this.$parent.name)
+      console.log(this.$root, '+++')
+    }
+  }
+};
+</script>
+
+<style>
+</style>
