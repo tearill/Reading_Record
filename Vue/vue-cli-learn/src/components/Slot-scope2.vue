a5e447d08ae4539fb505d939a39b113eea9c7e43
tearill <1105317666@qq.com>
2020-03-01 23:14:51 +0800

    学习作用域插槽

---
+++ Vue/vue-cli-learn/src/components/Slot-scope2.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <slot :data="languages">
+      <ul>
+        <li v-for="(item, index) in languages" :key="index">{{item}}</li>
+      </ul>
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Slot-scope2',
+  data() {
+    return {
+      languages: ['JavaScript', 'Go', 'Python', 'Flutter']
+    }
+  }
+};
+</script>
+
+<style>
+</style>
