83461337c04189f1d3a27185a317005bf0caefdc
tearill <1105317666@qq.com>
2020-03-01 23:18:59 +0800

    编译作用域

---
+++ Vue/vue-cli-learn/src/views/slot-scope.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>编译作用域</h1>
+    <cpn v-show="isShow"></cpn>
+  </div>
+</template>
+
+<script>
+import cpn from '../components/Slot-scope'
+
+export default {
+  name: 'slot-scope',
+  components: {
+    cpn
+  },
+  data() {
+    return {
+      isShow: true
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
