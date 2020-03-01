0c3ef0c5a79d555c7d6f68eee7554d8838deacf8
tearill <1105317666@qq.com>
2020-03-01 23:19:24 +0800

    作用域插槽

---
+++ Vue/vue-cli-learn/src/views/slot-scope2.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>作用域插槽</h1>
+    <cpn>
+      <!-- 目的是获取子组件的数据 -->
+      <template #default="{data}">
+        <!-- <span v-for="(item, index) in slot.data" :key="index">{{item}}</span> -->
+        <span>{{data.join(' - ')}}</span>
+      </template>
+    </cpn>
+  </div>
+</template>
+
+<script>
+import cpn from '../components/Slot-scope2'
+export default {
+  name: 'Component-5',
+  components: {
+    cpn
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
