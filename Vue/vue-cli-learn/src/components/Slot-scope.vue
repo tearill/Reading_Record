048b92b9f7e721dfa70c7aa0d4c657c1bc7d8c38
tearill <1105317666@qq.com>
2020-03-01 23:14:18 +0800

    学习编译作用域

---
+++ Vue/vue-cli-learn/src/components/Slot-scope.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>我是子组件</h2>
+    <p>我是子组件内容</p>
+    <button v-show="isShow"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Slot-scope',
+  data() {
+    return {
+      isShow: false
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
