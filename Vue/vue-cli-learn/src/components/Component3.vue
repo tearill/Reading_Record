cd2ff9af68074f6d1e119fc1c43b4083e151caeb
tearill <1105317666@qq.com>
2020-03-01 23:12:38 +0800

    组件3

---
+++ Vue/vue-cli-learn/src/components/Component3.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>我是子组件</div>
+</template>
+
+<script>
+export default {
+  name: "Component3",
+  data() {
+    return {
+      name: '我是子组件的data'
+    }
+  },
+  methods: {
+    showMsg() {
+      console.log("showMsg");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
