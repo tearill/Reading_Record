af505312a843e0e1264a25019f71755959d1eba1
tearill <1105317666@qq.com>
2020-03-03 23:13:12 +0800

    兄弟组件通信

---
+++ Vue/vue-cli-learn/src/views/Component/Component-6.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>兄弟组件传值</h1>
+    下面是两个兄弟组件:
+    <br />
+    <br />
+    <cpn3></cpn3>
+    <cpn4></cpn4>
+  </div>
+</template>
+
+<script>
+import cpn3 from '../../components/cpn-3'
+import cpn4 from '../../components/cpn-4'
+export default {
+  name: 'Component6',
+  components: {
+    cpn3,
+    cpn4
+  },
+  data() {
+    return {
+      muMessage: 'Hello brother component'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
