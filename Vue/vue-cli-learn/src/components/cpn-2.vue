a9a3daaf2322f8a80aef80b4bb8a84c29ad2e404
tearill <1105317666@qq.com>
2020-03-03 23:07:32 +0800

    eventBus定义

---
+++ Vue/vue-cli-learn/src/components/cpn-2.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <span>This is cpn2</span>
+    <br />
+    <input type="text" v-model="myMessagec" @input="passData">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cpn2',
+  data() {  
+    return {
+      myMessagec: this.$attrs.messagec,
+      test: this.$attrs.test
+    }
+  },
+  methods: {
+    passData() {
+      this.$emit('getCData', this.myMessagec)
+      this.$emit('getTest', this.test)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue/vue-cli-learn/src/config/bus.js
@@ -0,0 +1,4 @@
+// bus.js
+// 创建一个空的 Vue 实例并导出
+import Vue from 'vue'
+export default new Vue()
