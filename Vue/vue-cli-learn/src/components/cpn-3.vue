eb6007a3aa6af91c78ffc368e770088b6c103707
tearill <1105317666@qq.com>
2020-03-03 23:09:07 +0800

    con3

---
+++ Vue/vue-cli-learn/src/components/cpn-3.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <span>This is cpn3</span>
+    <br />
+    <label>
+      选择checkbox，可以触发组件 cpn4 的监听事件
+      <input type="checkbox" v-model="isChecked" @change="handleCheckbox" />
+    </label>
+  </div>
+</template>
+
+<script>
+import bus from '../config/bus'
+
+export default {
+  name: "cpn3",
+  data() {
+    return {
+      isChecked: false
+    }
+  },
+  methods: {
+    handleCheckbox() {
+      // 通过 bus 分发 getCheckboxStatus 事件
+      bus.$emit('getCheckboxStatus', this.isChecked)
+    }
+  }
+};
+</script>
+
+<style>
+</style>
