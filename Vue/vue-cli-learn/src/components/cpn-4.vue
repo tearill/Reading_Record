e6c47ed3df46553017044829cea7248fb6272a04
tearill <1105317666@qq.com>
2020-03-03 23:10:34 +0800

    cpn4

---
+++ Vue/vue-cli-learn/src/components/cpn-4.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <span>This is cpn4</span>
+    <br />
+    <p>这是组件 cpn3 中 isChecked 的值：{{isChecked}}</p>
+  </div>
+</template>
+
+<script>
+import bus from '../config/bus'
+
+export default {
+  name: 'cpn4',
+  data() {
+    return {
+      isChecked: false
+    }
+  },
+  mounted() {
+    this.getCheckboxStatus()
+  },
+  methods: {
+    getCheckboxStatus() {
+      // 通过 bus 监听 getCheckboxStatus 事件
+      bus.$on('getCheckboxStatus', data => {
+        this.isChecked = data
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
