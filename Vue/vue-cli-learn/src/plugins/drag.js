1ce3d333e0ace2faa43abe0690c0ab9d5bce5142
tearill <1105317666@qq.com>
2020-03-30 22:59:03 +0800

    添加注释

--- Vue/vue-cli-learn/src/plugins/drag.js
+++ Vue/vue-cli-learn/src/plugins/drag.js
@@ -1,15 +1,17 @@
 const fnDown = (el, ev) => {
     // console.log(ev);
-    let currentX = el.offsetLeft;
-    let currentY = el.offsetTop;
+    let currentX = el.offsetLeft; // 需要移动元素的左边距
+    let currentY = el.offsetTop; // 需要移动元素的上边距
 
-    let downX = ev.clientX;
-    let downY = ev.clientY;
+    let downX = ev.clientX; // 鼠标按下位置 X 坐标
+    let downY = ev.clientY; // 鼠标按下位置 Y 坐标
 
     const fnMove = (ev) => {
-        let left = ev.clientX - downX + currentX;
-        let top = ev.clientY - downY + currentY;
-
+        // 鼠标移动时计算每次移动的距离，并改变拖拽元素的定位
+        let left = ev.clientX - downX + currentX; // 拖动时鼠标的 clientX - 鼠标按下时的 clientX + 元素的初始 X
+        let top = ev.clientY - downY + currentY; // 拖动时鼠标的 clienY - 鼠标按下时的 clientY + 元素的初始 Y
+        el.style.cursor = 'move';
+        el.style.userSelect = 'none'
         el.style.left = left + 'px';
         el.style.top = top + 'px';
     }
